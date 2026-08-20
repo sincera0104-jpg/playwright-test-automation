3c6b79121d8f396908061978879c428b8d6a19b0
jiyeonjiyeon <sincera0104@gmail.com>
2026-08-20 17:12:18 +0900

    test: Playwright API 테스트 프로젝트 구성 및 상품 목록 테스트 추가

---
+++ tests/api/products.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+
+test('GET - 상품 목록 조회', async ({ request }) => {
+
+  const response = await request.get('https://fakestoreapi.com/products');
+
+  const body = await response.json();
+  
+  console.log(body);
+  
+  expect(response.status()).toBe(200);
+  expect(Array.isArray(body)).toBeTruthy();
+
+});
